e35597f2adf43c56afac4dbdd696229f1d856077
Mami. N <mamypoco@gmail.com>
2023-02-20 14:24:09 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import CustomForm from "./components/CustomForm";
+
+function App() {
+   const [count, setCount] = useState(0);
+
+   const addTask = (task) => {
+      console.log(task);
+   };
+
+   return (
+      <div className="container">
+         <header>
+            <h1>My Task List</h1>
+         </header>
+         <CustomForm addTask={addTask} />
+      </div>
+   );
+}
+
+export default App;
